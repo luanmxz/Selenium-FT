375a9ddc2ddfa7d1982fd7c5e3550e8b6de7377a
Luanmxz <54752531+luanmxz@users.noreply.github.com>
2024-06-11 17:01:57 -0300

    Test

--- src/main/java/com/furktech/Main.java
+++ src/main/java/com/furktech/Main.java
@@ -24,6 +24,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        logger.info("Testing remote repository");
+
         String agenda = args[0];
 
         try {
