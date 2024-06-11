2684462f223ed5b4ed504d3438ac00ce4a403b3a
luanmxz <luanmarcenemxz22@gmail.com>
2024-06-11 17:16:07 -0300

    REMOVED logger.info

--- src/main/java/com/furktech/Main.java
+++ src/main/java/com/furktech/Main.java
@@ -21,8 +21,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        logger.info("Testing remote repository");
-
         String agenda = args[0];
 
         try {
